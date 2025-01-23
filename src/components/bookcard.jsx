f057347c8e8b69b2f25c50bfb62e073b75f6f5f0
unknown <james.r@kalvium.community>
2025-01-23 13:12:15 +0530

    made changes

--- src/components/bookcard.jsx
+++ src/components/bookcard.jsx
@@ -1,1 +1,16 @@
 // write the book component code here
+import React from 'react';
+
+function BookCard({ image, name, genre, author }) {
+  return (
+    <div
+      className="book-card" style={{display: 'grid', padding:10, margin: 10 }}   >
+      <img src={image} alt={name} style={{height:400 ,width:300}} />
+      <h2>{name}</h2>
+      <p><strong>Genre:</strong> {genre}</p>
+      <p> <strong>Author:</strong> {author} </p>
+    </div>
+  );
+}
+
+export default BookCard;
